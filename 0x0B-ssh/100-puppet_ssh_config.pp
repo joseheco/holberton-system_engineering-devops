f73a4f6a6eb0c84ab76633fb82b24a6f51ad9764
joseheco <jferhc@gmail.com>
2021-08-18 19:52:11 -0500

    AGAIN

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,12 @@
 # Client ssh_config file that we can connect to the server without a pass
-file {'Configuration File':
-  ensure => file,
-  name   => 'ssh_config',
-  path   => '/etc/ssh/ssh_config'
-}
-
 file_line { 'Declare identity file':
-  ensure => file,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '  IdentityFile ~/.ssh/holberton'
+  line   => 'IdentityFile ~/.ssh/holberton'
 }
 
 file_line { 'Turn off passwd auth':
-  ensure => file,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '  PasswordAuthentication no'
+  line   => 'PasswordAuthentication no'
 }
-
