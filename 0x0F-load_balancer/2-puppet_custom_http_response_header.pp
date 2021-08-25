f9354c7d594457c4ca34ab7b8e0d8f7252fbde87
joseheco <jferhc@gmail.com>
2021-08-24 20:24:32 -0500

    install

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,28 @@
 # Use puppet to creat a custom HTTP header response
 exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+    command  => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+    ensure  => 'present',
+    require => Exec['apt-get-update'],
 }
 
 file_line { 'add header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => "\n\tadd_header X-Served-By \$hostname;",
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'listen 80 default_server;',
+    line   => "\n\tadd_header X-Served-By \$hostname;",
 }
 
 file { '/var/www/html/index.html':
-  content => 'Holberton School',
-  require => Package['nginx'],
+    ensure  => 'present',
+    content => 'Holberton School',
+    require => Package['nginx'],
 }
 
 service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
 }
