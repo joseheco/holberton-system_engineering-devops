db379faa4e548838e3574172732be2e0f870e95a
joseheco <jferhc@gmail.com>
2021-08-24 20:30:09 -0500

    install

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,15 +4,23 @@ exec { 'apt-get-update':
 }
 
 package { 'nginx':
-    ensure  => 'present',
+    ensure  => installed,
     require => Exec['apt-get-update'],
+}
+
+file_line { 'add':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
 file_line { 'add header':
     ensure => 'present',
     path   => '/etc/nginx/sites-available/default',
     after  => 'listen 80 default_server;',
-    line   => "\n\tadd_header X-Served-By \$hostname;",
+    line   => "add_header X-Served-By $hostname;",
 }
 
 file { '/var/www/html/index.html':
