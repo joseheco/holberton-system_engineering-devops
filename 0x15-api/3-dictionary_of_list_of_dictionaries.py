eb593014979cb9d53127e44ebe01c22c657e7314
joseheco <jferhc@gmail.com>
2021-09-13 20:30:36 -0500

    API

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""python script , use REST API"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    employee = requests.get(url).json().get('name')
+
+    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        argv[1])
+    todos = requests.get(url_todos).json()
+    done = [todo for todo in todos if todo.get('completed')]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee, len(done), len(todos)))
+    for a in done:
+        print("\t ", a.get('title'))
