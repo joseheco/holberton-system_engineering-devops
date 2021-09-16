4f143b681eb55dc44f794672fa3ceb2854ec8930
joseheco <jferhc@gmail.com>
2021-09-16 07:46:54 -0500

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,12 @@
-# Use puppet to creat a custom HTTP header response
-exec { 'apt-get-update':
-    command  => '/usr/bin/apt-get update',
-}
+# header response, but with Puppet.
 
-package { 'nginx':
-    ensure  => installed,
-    require => Exec['apt-get-update'],
-}
-
-file_line { 'add':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'add header':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server;',
-    line    => 'add_header X-Served-By $hostname;',
-    require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-    ensure  => 'present',
-    content => 'Holberton School',
-    require => Package['nginx'],
-}
-
-service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
+exec { 'setting up nginx':
+  command  => 'sudo apt-get -y update;
+                sudo apt-get -y upgrade;
+                sudo apt-get -y install nginx;
+                echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;
+                sudo sed -i "/server_name _;/ a \\\trewrite ^/redirect_me https://www.google.com permanent;" /etc/nginx/sites-available/default
+                sudo sed -i "11i \\\t add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/nginx.conf;
+                sudo service nginx restart',
+  provider => shell,
 }
