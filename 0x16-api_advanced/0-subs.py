dfec4de7b7dab652cfc5c83027d5b41557fab197
joseheco <jferhc@gmail.com>
2021-09-28 21:33:54 -0500

    add new api subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write function that queries the Reddit API
+    and return the number of subcribers.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """ return number of subscribers"""
+    info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers={'User-Agent': 'Custom'},
+            allow_redirects=False)
+    if info.status_code == 200:
+        return info.json().get('data').get('subscribers')
+    return 0
+
