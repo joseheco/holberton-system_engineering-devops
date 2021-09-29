c76858e3c0f21a02ec8d5885a45b12a764e37fc0
joseheco <jferhc@gmail.com>
2021-09-29 18:49:26 -0500

    puppet fix problem phpp to php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix wordpress
 exec { 'fix-wordpress':
-    command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-    path    => ['/bin', '/usr/bin', '/usr/sbin']
+    command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/g  /var/www/html/wp-settings.php',
+    path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
