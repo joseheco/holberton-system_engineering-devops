bb7e1282016b37107a7f394f0de589087b7389ea
joseheco <jferhc@gmail.com>
2021-09-28 21:35:55 -0500

    add new api subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,12 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ return number of subscribers"""
-    info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers={'User-Agent': 'Custom'},
-            allow_redirects=False)
+    info = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit), headers={'User-Agent': 'Custom'},
+                        allow_redirects=False)
     if info.status_code == 200:
         return info.json().get('data').get('subscribers')
     return 0
-
